e02d2e915673f2f349068745c3179e1428ca9d02
haoli <328822014@qq.com>
2017-05-23 19:25:23 +0800

    cc

--- game-server/release.sh
+++ game-server/release.sh
@@ -4,19 +4,17 @@
 if [ "$1" == "production" ]
 then
     echo 'production start update'
-    ssh root@106.14.94.54 "cd /home/work ; ./game.sh ;exit";
-    echo '======== 服务器106.14.94.54 拉取代码完毕 ========'
+    ssh root@47.94.45.41 "cd /home/work ; ./game.sh ;exit";
+    echo '======== 服务器47.94.45.41 拉取代码完毕 ========'
 
     ssh root@106.15.45.74 "cd /home/work ; ./game.sh ;exit";
     echo '======== 入口服务器 106.15.45.74 拉取完毕 并启动完成========'
 else
     echo 'stage start update'
 
-    ssh root@106.14.240.205 "cd /home/work ; ./game.sh ;exit";
-    echo '======== 服务器 106.14.240.205 拉取代码完毕 ========'
+    ssh root@47.94.45.41 "cd /home/work ; ./game.sh ;exit";
+    echo '======== 服务器47.94.45.41 拉取代码完毕 ========'
 
-    ssh root@106.15.38.247 "cd /home/work ; ./game.sh ;exit";
-    echo '======== 入口服务器 106.15.38.247 拉取完毕 并启动完成========'
 fi
 
 
