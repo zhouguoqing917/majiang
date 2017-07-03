3ba093cf0fc5e593c12123b4a09951c182e0cead
doga <328822014@qq.com>
2017-07-03 23:12:44 +0800

    cc

--- web-login-server/db/gameuser.js
+++ web-login-server/db/gameuser.js
@@ -50,11 +50,11 @@ schema.statics={
     register:async function(obj){
         const data=await this.findOne({ openid : obj.openid });
         if(data){
-            this.update({openid:obj.openid}, {$set : obj}, {upsert : true}).exec();
+            await this.update({openid:obj.openid}, {$set : obj}, {upsert : true}).exec();
         }else{
-            let n=await autoNumber.getNewNumber(modelName);
+            let n = await autoNumber.getNewNumber(modelName);
             let gameUser=Object.assign({},obj,{id:n,roomCard:3});
-            this.create(gameUser,(error)=>{
+            await this.create(gameUser,(error)=>{
                 if(error) {
                     console.log(`新增用户失败 ${error.msg}`);
                 } else {
